1f0b7a3bfdc87b634f5df52f20d5b1274f325641
Fakorede Damilola <dfakorede29@gmail.com>
2020-11-04 17:52:39 +0100

    aaded the speakers

--- script.js
+++ script.js
@@ -6,6 +6,26 @@ let hoursVal = document.querySelector(".hoursVal");
 let minutesVal = document.querySelector(".minutesVal");
 let secondsVal = document.querySelector(".secondsVal");
 let deadlineDate = new Date(deadline);
+let speakers = [
+  {
+    img: "MARIAM\ MOMODU.jpg",
+    name: "MARIAM MOMODU",
+    info: "Mariam is a Doctoral Candidate and Vanier Scholar at the University of Toronto Law Faculty, where she specializes in international trade law and law and development. She obtained a master’s degree in law from the University of Cambridge on a Commonwealth Shared Scholarship. She obtained her undergraduate law degree from the University of Ibadan, Nigeria.<br/><br/> Mariam is the founder of GetIn Education Consulting; a social enterprise that equips young people from underrepresented backgrounds with the skills and knowledge required to access quality education and compete globally. GetIn also advocates for quality education in developing countries.<br/><br/>Mariam has received several recognitions for her work in education and development. She was recognised by McKinsey and Co. as one of the 40 Next Generation Women Leaders in Nigeria and was a delegate at the World Youth Forum in 2019."
+  }
+]
+let speakerLink = document.querySelectorAll(".speakerLink")
+let speakerName = document.querySelector(".speakerName")
+let speakerImg = document.querySelector(".speakerImg")
+let speakerInfo = document.querySelector(".speakerInfo")
+Array.from(speakerLink).forEach(speaker => {
+
+  speaker.addEventListener("click", function () {
+    let click = this.dataset.id
+    speakerInfo.innerHTML = speakers[click].info
+    speakerName.innerHTML = speakers[click].name
+    speakerImg.src = speakers[click].img
+  })
+})
 function createValue(time) {
   let days = Math.floor(time / (3600 * 24));
   let hours = Math.floor((time % (3600 * 24)) / 3600);
