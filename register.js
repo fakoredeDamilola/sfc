f58fe72d6012f014db5553c20e6d3798f2c89e48
Fakorede Damilola <dfakorede29@gmail.com>
2020-11-16 04:56:10 +0100

    latest

--- register.js
+++ register.js
@@ -1,17 +1,7 @@
 let form = document.querySelector("form")
+
 let inputForm = document.querySelectorAll(".form-group input")
-var firebaseConfig = {
-    apiKey: "AIzaSyBjtTuXY-B3R_-9TRxJduCQwS-_MR8odp4",
-    authDomain: "finxposition.firebaseapp.com",
-    databaseURL: "https://finxposition.firebaseio.com",
-    projectId: "finxposition",
-    storageBucket: "finxposition.appspot.com",
-    messagingSenderId: "306330836144",
-    appId: "1:306330836144:web:9a3a23a40880c2d799af75",
-    measurementId: "G-HCQ6H4BYSF"
-};
-firebase.initializeApp(firebaseConfig);
-var db = firebase.firestore();
+
 
 let selectForm = document.querySelectorAll(".form-group select")
 let dataValue = [false, false, false, false, false, false, false, false]
@@ -26,17 +16,19 @@ let newUser = {
     gradeLevelOfInterest: "",
     expectation: ""
 }
-
 form.addEventListener("submit", function (e) {
     e.preventDefault()
     console.log(inputForm)
     Array.from(inputForm).forEach(input => {
         newUser[input.id] = input.value
+        input.value = ""
     })
     Array.from(selectForm).forEach(select => {
         newUser[select.id] = select.value
+        select.value = ""
     })
     newUser.expectation = textForm.value
+    textForm.value = ""
     Object.keys(newUser).forEach((key, index) => {
         if (newUser[key] === "") {
             dataValue[index] = false
@@ -46,21 +38,31 @@ form.addEventListener("submit", function (e) {
     })
     if (dataValue.every(value => value === true)) {
         saveResponse(newUser)
+        console.log(newUser)
     } else {
         alert("Please fill all the information. Go through the form again")
-
+        console.log(newUser)
     }
 })
 
 
-function saveResponse(obj) {
-    //get responses
-    // db.collection("responses").get().then(snap => {
-    //     snap.forEach(data => {
-    //         console.log(data.data())
-    //     })
-    // })
-    db.collection("responses").add(obj)
-        .then(doc => alert("Data completed, thank you for your time, we are looking forward to meeting you at finXposition 1.0"))
-    window.location.replace("https://sfcui.org.ng/event/index.html");
+async function saveResponse(obj) {
+    let url = "https://script.google.com/macros/s/AKfycbwARmZ351UkrwFtnBZ7hsqjeEeaw__xtEQclWUypaC3y_gq2Hc/exec"
+
+    $.ajax({
+        url: url,
+        method: "POST",
+        dataType: "xml",
+        data: obj,
+        statusCode: {
+            0: function () {
+                console.log("sucess 1")
+            },
+            200: function () {
+                console.log("success 2")
+            }
+        }
+    })
+
+
 }
